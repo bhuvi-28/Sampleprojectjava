3986b661291e885a3ef2c06dfb7960a6a8950b0e
bhuvanamanchikatla <bhuvanamanchikatla@gmail.com>
2025-03-02 02:53:25 +0530

    backend code

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/users")
  @CrossOrigin(origins = "http://localhost:3000")
